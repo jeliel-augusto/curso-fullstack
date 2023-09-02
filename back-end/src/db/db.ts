c46a3c5730c99bbca21739a29b9fbbdde69517e5
Jeliel Augusto <jeliel.augusto10@gmail.com>
2023-09-02 12:18:10 -0400

    config: update db config

--- back-end/src/db/db.ts
+++ back-end/src/db/db.ts
@@ -6,8 +6,8 @@ export const knexConnection = knex({
   connection: {
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT!,
-    user: "root",
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "bd_games",
+    database: process.env.DB_DATABASE,
   },
 });
